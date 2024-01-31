6913fe1ce6737eb21bbe29dd2e35a778c55e7293
Germán Mora <dmm71317@gmail.com>
2024-01-31 16:38:11 -0600

    Estructura Inicial del Proyecto

---
+++ src/Timer.svelte
@@ -0,0 +1,19 @@
+<script>
+
+    let titulo = "Pomodoro Timer Aplicación";
+
+</script>
+
+<section>
+
+    <h1>{titulo}</h1>
+
+</section>
+
+<style>
+
+    h1 {
+        font-size: 19px;
+    }
+
+</style>
